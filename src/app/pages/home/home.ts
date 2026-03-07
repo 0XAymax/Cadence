4506aa31ef44eb4bdb643867ff91b8f4cf96ac3d
0XAymax <aymanderrouich3@gmail.com>
2026-03-07 20:25:29 +0000

    Landing page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { HlmButtonImports } from '../../../components/ui/button/src';
 
 @Component({
   selector: 'app-home',
-  template: `<div class="mx-auto max-w-6xl px-6 py-20"></div>`,
+  imports: [NgOptimizedImage, RouterLink, ...HlmButtonImports],
+  templateUrl: './home.html',
 })
 export class Home {}
