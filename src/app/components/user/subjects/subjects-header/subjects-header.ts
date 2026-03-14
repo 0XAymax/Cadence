e3f9d73cace8f3894312daaf0c4f413c29664706
0XAymax <aymanderrouich3@gmail.com>
2026-03-14 23:31:52 +0000

    Adding subject

--- src/app/components/user/subjects/subjects-header/subjects-header.ts
+++ src/app/components/user/subjects/subjects-header/subjects-header.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { HlmLabelImports } from '@spartan-ng/helm/label';
+import { HlmSheetImports } from '@spartan-ng/helm/sheet';
 import { LucideAngularModule, Plus } from 'lucide-angular';
 
 @Component({
   selector: 'app-subjects-header',
-  imports: [LucideAngularModule, ...HlmButtonImports],
+  imports: [
+    LucideAngularModule,
+    ...HlmButtonImports,
+    ...HlmInputImports,
+    ...HlmLabelImports,
+    ...HlmSheetImports,
+  ],
   templateUrl: './subjects-header.html',
 })
 export class SubjectsHeader {
