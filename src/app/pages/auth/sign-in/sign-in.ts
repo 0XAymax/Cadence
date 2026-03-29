f675b54ab6d0532da16603481960bfb22ab75b3b
0XAymax <aymanderrouich3@gmail.com>
2026-03-29 21:36:42 +0100

    Setting Up sign-in/up forms

--- src/app/pages/auth/sign-in/sign-in.ts
+++ src/app/pages/auth/sign-in/sign-in.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { LucideAngularModule, Eye, EyeOff, BookOpen } from 'lucide-angular';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmInputImports } from '@spartan-ng/helm/input';
@@ -7,7 +7,7 @@ import { HlmCardImports } from '@spartan-ng/helm/card';
 import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 import { RouterLink } from '@angular/router';
 import { LogoComponent } from '@app/components/logo/Logo';
-
+import { form, FormField, required, email, submit, FormRoot } from '@angular/forms/signals';
 @Component({
   selector: 'app-sign-in',
   imports: [
@@ -19,17 +19,42 @@ import { LogoComponent } from '@app/components/logo/Logo';
     ...HlmSeparatorImports,
     RouterLink,
     LogoComponent,
+    FormField,
+    FormRoot,
   ],
   templateUrl: './sign-in.html',
 })
 export class SignIn {
+  loginModel = signal({ email: '', password: '' });
   readonly Eye = Eye;
   readonly EyeOff = EyeOff;
   readonly BookOpen = BookOpen;
 
   showPassword = false;
 
+  loginForm = form(this.loginModel, (schema) => {
+    required(schema.email, { message: 'Email is required' });
+    email(schema.email, { message: 'Please enter a valid email address' });
+    required(schema.password, { message: 'Password is required' });
+  },{
+  submission: {
+    action: async () => {
+      const credentials = this.loginModel();
+      console.log('Logging in with:', credentials);
+    }
+  }});
+
   togglePassword() {
     this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.loginForm().invalid()) {
+      alert('Please fix the errors in the form before submitting.');
+      return;
+    }
+    const credentials = this.loginModel();
+    console.log('Logging in with:', credentials);
   }
 }
--- src/app/pages/auth/sign-up/sign-up.ts
+++ src/app/pages/auth/sign-up/sign-up.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideAngularModule, Eye, EyeOff } from 'lucide-angular';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
@@ -6,6 +6,7 @@ import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
 import { HlmCardImports } from '@spartan-ng/helm/card';
 import { LogoComponent } from '@app/components/logo/Logo';
+import { form, required, email, FormRoot, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-sign-up',
@@ -17,14 +18,31 @@ import { LogoComponent } from '@app/components/logo/Logo';
     ...HlmLabelImports,
     ...HlmCardImports,
     LogoComponent,
-  ],
+    FormRoot,
+    FormField
+],
   templateUrl: './sign-up.html',
 })
 export class SignUp {
+  readonly singupModel = signal({ firstName: '', lastName: '', email: '', password: '' });
   readonly Eye = Eye;
   readonly EyeOff = EyeOff;
 
   showPassword = false;
+  SignUpForm = form(this.singupModel, (schema) => {
+    required(schema.firstName, {message: "First Name is required"});
+    required(schema.lastName, { message: 'Last Name is required' });
+    required(schema.email, { message: 'Email is required' });
+    email(schema.email, { message: 'Please enter a valid email adress' });
+    required(schema.password, { message: 'Password is required' });
+  }, {
+    submission: {
+      action: async() => {
+        const credentials = this.singupModel();
+        console.log("Registered Data", credentials)
+      },
+    }
+  });
 
   togglePassword() {
     this.showPassword = !this.showPassword;
