9f2d88f719f32fe83066471eba01cd2afc47d6d4
0XAymax <aymanderrouich3@gmail.com>
2026-04-05 14:28:50 +0100

    Adding Logout

---
+++ src/app/pages/user/logout/logout.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/core/services/auth.service';
+import { HlmAlertImports } from '@spartan-ng/helm/alert';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { LucideAngularModule, TriangleAlert } from 'lucide-angular';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-logout',
+  imports: [HlmAlertImports, HlmButtonImports, LucideAngularModule],
+  templateUrl: './logout.html',
+})
+export class LogoutComponent {
+  private authService = inject(AuthService);
+  private location = inject(Location);
+
+  readonly TriangleAlert = TriangleAlert;
+
+  confirmLogout() {
+    this.authService.logout();
+  }
+
+  cancel() {
+    this.location.back();
+  }
+}
